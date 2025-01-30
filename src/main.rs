1d6817341175404bc9293e8275ce198ca993cb5c
megawonzz9 <moossotimoteo@gmail.com>
2025-01-30 11:38:13 +0100

    gowno unstable

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,67 @@ fn main() -> Result<(), Box<dyn Error>> {
         eprint!("yo need atleast 2 args");
         exit(1);
     }
-    if line.contains(&"-f") {
-        let line_f = &line[1..];
-        if let Some(file) = line_f.iter().position(|&minus| minus == "-f") {
-            let del_args = &line_f[file..file + 2];
-            let argf: Vec<&str> = line_f
-                .iter()
-                .filter(|&&x| !del_args.contains(&x))
-                .copied()
-                .collect();
-            let _ = with_file(line_f[file + 1], &argf);
-        } else {
-            eprintln!("bad use o -")
+    if let Some(word) = line.iter().find(|w| w.starts_with('-')) {
+        match *word {
+            "-f" => with_file(&line)?,
+            "-i" => {}
+            _ => piped(&line)?,
         }
     } else {
-        let _ = piped(&line[..]);
+        let _ = piped(&line);
     }
+
+    //
+    //if line.contains(&"-f") {
+    //    let line_f = &line[1..];
+    //    if let Some(file) = line_f.iter().position(|&minus| minus == "-f") {
+    //        let del_args = &line_f[file..file + 2];
+    //        let argf: Vec<&str> = line_f
+    //            .iter()
+    //            .filter(|&&x| !del_args.contains(&x))
+    //            .copied()
+    //            .collect();
+    //        let _ = with_file(line_f[file + 1], &argf);
+    //    } else {
+    //        eprintln!("bad use o -")
+    //    }
+    //} else {
+    //    let _ = piped(&line[..]);
+    //}
 
     Ok(())
 }
 
-fn with_file(file: &str, args: &[&str]) -> Result<(), Box<dyn Error>> {
-    let filef = File::open(file)?;
-    let reader = BufReader::new(filef);
-    for (line_number, line) in reader.lines().enumerate() {
-        let line = line?;
-        if args.iter().all(|arg| line.contains(arg)) {
-            println!("{}: {}", line_number + 1, &line);
+fn with_file(line: &[&str]) -> Result<(), Box<dyn Error>> {
+    let line_f = &line[1..];
+    if let Some(filez) = line_f.iter().position(|&minus| minus == "-f") {
+        let del_args = &line_f[filez..filez + 2];
+        let args: Vec<&str> = line_f
+            .iter()
+            .filter(|&&x| !del_args.contains(&x))
+            .copied()
+            .collect();
+        let filename = line_f[filez + 1];
+        let filef = File::open(filename)?;
+        let reader = BufReader::new(filef);
+        for (line_number, line) in reader.lines().enumerate() {
+            let line = line?;
+            if args.iter().all(|arg| line.contains(arg)) {
+                println!("{}: {}", line_number + 1, &line);
+            }
         }
+    } else {
+        eprintln!("bad use o -f")
     }
+    //let filename = line_f[filez + 1];
+    //let filef = File::open(filename)?;
+    //let reader = BufReader::new(filef);
+    //for (line_number, line) in reader.lines().enumerate() {
+    //    let line = line?;
+    //    if args.iter().all(|arg| line.contains(arg)) {
+    //        println!("{}: {}", line_number + 1, &line);
+    //    }
+    //}
 
     Ok(())
 }
